121dc66452dc1b2205494ebc6fc77440144fdb63
Sander De la Marche <sander@sumocoders.be>
2022-04-01 12:52:49 +0200

    Replace third party enum implementation with native one

--- src/Inotify/InotifyEventCodeEnum.php
+++ src/Inotify/InotifyEventCodeEnum.php
@@ -1,77 +1,43 @@
 <?php
-
+// phpcs:ignoreFile
 declare(strict_types=1);
 
 namespace Inotify;
 
 use InvalidArgumentException;
-use MyCLabs\Enum\Enum;
-use UnexpectedValueException;
 
-/**
- * @method static InotifyEventCodeEnum ON_ACCESS()
- * @method static InotifyEventCodeEnum ON_MODIFY()
- * @method static InotifyEventCodeEnum ON_ATTRIB();
- * @method static InotifyEventCodeEnum ON_CLOSE_WRITE()
- * @method static InotifyEventCodeEnum ON_CLOSE_NOWRITE()
- * @method static InotifyEventCodeEnum ON_OPEN()
- * @method static InotifyEventCodeEnum ON_MOVED_FROM()
- * @method static InotifyEventCodeEnum ON_MOVED_TO()
- * @method static InotifyEventCodeEnum ON_CREATE()
- * @method static InotifyEventCodeEnum ON_DELETE()
- * @method static InotifyEventCodeEnum ON_DELETE_SELF()
- * @method static InotifyEventCodeEnum ON_MOVE_SELF()
- * @method static InotifyEventCodeEnum ON_UNMOUNT()
- * @method static InotifyEventCodeEnum ON_Q_OVERFLOW()
- * @method static InotifyEventCodeEnum ON_IGNORED()
- * @method static InotifyEventCodeEnum ON_CLOSE()
- * @method static InotifyEventCodeEnum ON_MOVE()
- * @method static InotifyEventCodeEnum ON_ALL_EVENTS()
- * @method static InotifyEventCodeEnum ON_ONLYDIR()
- * @method static InotifyEventCodeEnum ON_DONT_FOLLOW()
- * @method static InotifyEventCodeEnum ON_MASK_ADD()
- * @method static InotifyEventCodeEnum ON_ISDIR()
- * @method static InotifyEventCodeEnum ON_ONESHOT()
- * @method static InotifyEventCodeEnum ON_CLOSE_NOWRITE_HIGH()
- * @method static InotifyEventCodeEnum ON_OPEN_HIGH()
- * @method static InotifyEventCodeEnum ON_CREATE_HIGH()
- * @method static InotifyEventCodeEnum ON_DELETE_HIGH()
- * @method static InotifyEventCodeEnum UNKNOWN()
- */
-class InotifyEventCodeEnum extends Enum
+enum InotifyEventCodeEnum: int
 {
-    private const ON_ACCESS = 1;
-    private const ON_MODIFY = 2;
-    private const ON_ATTRIB = 4;
-    private const ON_CLOSE_WRITE = 8;
-    private const ON_CLOSE_NOWRITE = 16;
-    private const ON_OPEN = 32;
-    private const ON_MOVED_FROM = 64;
-    private const ON_MOVED_TO = 128;
-    private const ON_CREATE = 256;
-    private const ON_DELETE = 512;
-    private const ON_DELETE_SELF = 1024;
-    private const ON_MOVE_SELF = 2048;
-    private const ON_UNMOUNT = 8192;
-    private const ON_Q_OVERFLOW = 16384;
-    private const ON_IGNORED = 32768;
-    private const ON_CLOSE = 24;
-    private const ON_MOVE = 192;
-    private const ON_ALL_EVENTS = 4095;
-    private const ON_ONLYDIR = 16777216;
-    private const ON_DONT_FOLLOW = 33554432;
-    private const ON_MASK_ADD = 536870912;
-    private const ON_ISDIR = 1073741824;
-    private const ON_ONESHOT = 2147483648;
+    case ON_ACCESS = 1;
+    case ON_MODIFY = 2;
+    case ON_ATTRIB = 4;
+    case ON_CLOSE_WRITE = 8;
+    case ON_CLOSE_NOWRITE = 16;
+    case ON_OPEN = 32;
+    case ON_MOVED_FROM = 64;
+    case ON_MOVED_TO = 128;
+    case ON_CREATE = 256;
+    case ON_DELETE = 512;
+    case ON_DELETE_SELF = 1024;
+    case ON_MOVE_SELF = 2048;
+    case ON_UNMOUNT = 8192;
+    case ON_Q_OVERFLOW = 16384;
+    case ON_IGNORED = 32768;
+    case ON_CLOSE = 24;
+    case ON_MOVE = 192;
+    case ON_ALL_EVENTS = 4095;
+    case ON_ONLYDIR = 16777216;
+    case ON_DONT_FOLLOW = 33554432;
+    case ON_MASK_ADD = 536870912;
+    case ON_ISDIR = 1073741824;
+    case ON_ONESHOT = 2147483648;
+    case ON_CLOSE_NOWRITE_HIGH = 1073741840;
+    case ON_OPEN_HIGH = 1073741856;
+    case ON_CREATE_HIGH = 1073742080;
+    case ON_DELETE_HIGH = 1073742336;
+    case UNKNOWN = 0;
 
-    private const ON_CLOSE_NOWRITE_HIGH = 1073741840;
-    private const ON_OPEN_HIGH = 1073741856;
-    private const ON_CREATE_HIGH = 1073742080;
-    private const ON_DELETE_HIGH = 1073742336;
-
-    private const UNKNOWN = 0;
-
-    public static $constants = [
+    public const DESCRIPTIONS = [
         0 => ['UNKNOWN', 'Unknown code.'],
         1 => ['ON_ACCESS', 'File was accessed (read)'],
         2 => ['ON_MODIFY', 'File was modified'],
@@ -102,21 +68,12 @@ class InotifyEventCodeEnum extends Enum
         2147483648 => ['ON_ONESHOT', 'Monitor pathname for one event, then remove from watch list.'],
     ];
 
-    public static function getCodeDescription(int $code): string
+    public function getDescription(): string
     {
-        if (!isset(self::$constants[$code])) {
+        if (!isset(self::DESCRIPTIONS[$this->value])) {
             throw new InvalidArgumentException('Unknown code');
         }
 
-        return implode(' - ', self::$constants[$code]);
-    }
-
-    public static function createFromMask(int $code): self
-    {
-        try {
-            return new self($code);
-        } catch (UnexpectedValueException $exception) {
-            return self::UNKNOWN();
-        }
+        return implode(' - ', self::DESCRIPTIONS[$this->value]);
     }
 }
